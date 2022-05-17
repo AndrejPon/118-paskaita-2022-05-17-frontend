cbd6827280935bb0ecb2944a17127e6d63b41592
Andrejus Ponomariovas <andrejus.pon@gmail.com>
2022-05-17 08:32:03 +0100

    Header component added

---
+++ src/components/Header/Header.style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+  background: lightblue;
+  font-size: 2rem;
+  font-weight: bold;
+  height: 5rem;
+  line-height: 5rem;
+  text-align: center;
+`;
